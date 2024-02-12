e3f000794e642ef7decbac3a755925342e0d963c
OluwatomisinAtamamen <47326823+OluwatomisinAtamamen@users.noreply.github.com>
2024-02-12 21:20:25 +0000

    UI Design

--- client/scripts/main.mjs
+++ client/scripts/main.mjs
@@ -1,5 +1,13 @@
 function addHandlers(){
+  const formsubmit = document.querySelector('#enter');
+  formsubmit.addEventListener('click', formHandler);
+}
+
+function loginHandler(){
     
+}
+
+function signupHandler(){
 }
 
 window.addEventListener('load', addHandlers);
